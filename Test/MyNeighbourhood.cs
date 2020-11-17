516d3d684f11167af0270f5ddd95dd1813526de5
Collins <ge37mof@mytum.de>
2020-11-17 10:21:05 +0100

    second initial commit

---
+++ Test/MyNeighbourhood.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Test
+{
+    internal class MyNeighbourhood
+    {
+        public string Entity { get; set; }
+        public int Wall { get; set; }
+        public int Stair { get; set; }
+        public int Slab { get; set; }
+        public int FurnishingElement { get; set; }
+        public int Column { get; set; }
+        public int Flowterminal { get; set; }
+        public int FlowSegment { get; set; }
+        public int FlowFitting { get; set; }
+        public int FlowController { get; set; }
+        public int DistributionControlElement { get; set; }
+        public int Window { get; set; }
+        public int Door { get; set; }
+
+    }
+}
+
